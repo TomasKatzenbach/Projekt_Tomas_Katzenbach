5fe5039f01cb4ada4193d313c82a52febc7f7049
TomasKatzenbach <tomas.katzenbach@student.ukf.sk>
2024-04-01 21:11:15 +0200

    Pridaný header

---
+++ header.php
@@ -0,0 +1,26 @@
+<header>
+<nav class="navbar navbar-expand-lg fixed-top">
+  <a class="navbar-brand" href="index.html">CodeOcean</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon">
+      <i class="fas fa-bars" style="color:#fff; font-size:28px;"></i>
+  </span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">Domov</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="produkty.html">Produkty</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="o-nas.html">O nás</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="kontakt.html">Kontakt</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</header>
