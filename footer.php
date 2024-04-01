c28772eaedd85bdaca00fc7c1bc6c58dfdf45af3
TomasKatzenbach <tomas.katzenbach@student.ukf.sk>
2024-04-01 21:10:04 +0200

    Pridaný footer

---
+++ footer.php
@@ -0,0 +1,38 @@
+<footer class="text-dark text-center">
+  <div class="container d-md-flex justify-content-md-between">
+    <!-- Prvý stĺpec - autorské práva -->
+    <div class="mb-3">
+      <p class="mb-0">&copy; 2023 CodeOcean.</p>
+      <p>Všetky práva vyhradené.</p>
+    </div>
+    
+    <!-- Stredný stĺpec - sociálne siete a ikony -->
+    <div class="text-center">
+      <p>Sociálne Siete</p>
+      <ul class="list-inline social-icons">
+        <li class="list-inline-item mx-2">
+          <a href="https://www.facebook.com/tomas.katzenbach" target="_blank">
+            <i class="fab fa-facebook-f fa-2x"></i>
+          </a>
+        </li>
+        <li class="list-inline-item mx-2">
+          <a href="https://twitter.com/tomikaas1" target="_blank">
+            <i class="fab fa-twitter fa-2x"></i>
+          </a>
+        </li>
+        <li class="list-inline-item mx-2">
+          <a href="https://www.instagram.com/tomaash.wav/" target="_blank">
+            <i class="fab fa-instagram fa-2x"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+    
+    <!-- Pravý stĺpec - mailto a telefón -->
+    <div class="mb-3">
+      <p class="mb-0">Email: <a href="mailto:tomaash69@gmail.com">codeocean@codeocean.com</a></p>
+      <p class="mb-0">Tel: +421 944 645 429</p>
+      <p>Email podpora 24/7</p>
+    </div>
+  </div>
+</footer>
